02633505f49e742ce23fdc0a2fba936676a5ccbd
Logeshwaranc01 <clogesh09112003@gmail.com>
2024-07-09 16:03:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('off').addEventListener('click',function(){
+    document.getElementById("image").src="https://images.unsplash.com/photo-1519414442781-fbd745c5b497?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHx8"
+})    
+document.getElementById('on').addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/162515751/photo/moon-over-mountains.jpg?s=612x612&w=0&k=20&c=mKsWiAqN8lOGl6bdnDg9ssY25RSJJbSOk3g0MuDZNXw="
+})
+
